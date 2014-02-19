aa7c137454461199d6946d476fff3c657be27b3e
yukinagae <yuki.nagae1130@gmail.com>
2014-02-19 10:00:30 +0900

    テストコードも作成し、全てグリーンになった。

--- src/main/scala/slavatar/Core.scala
+++ src/main/scala/slavatar/Core.scala
@@ -1,5 +1,36 @@
 package slavatar
 
+import java.security.MessageDigest
+import java.math.BigInteger
+
 object Core {
+
+  val gravatar_base_url = ".gravatar.com/avatar/"
+
+  def md5sum(s: String): String = {
+    val digest = MessageDigest.getInstance("MD5")
+    digest.reset
+    val h = digest.digest(s.getBytes("UTF-8"))
+    val b = new BigInteger(1, h)
+    b.toString(16)
+  }
+
+  def genhash(email: String): String = {
+    md5sum(email.trim.toLowerCase)
+  }
+
+  def genparams(size: Int, default: String, rating: String): String = {
+    "?s=%s&r=%s&d=%s&".format(size, rating, default)
+  }
+
+  def gravatar(email: String, size: Int = 50, default: String = "retro", rating: String = "pg", https: Boolean = true): String = {
+    val base_url = s"${gravatar_base_url}${genhash(email)}.jpg${genparams(size, default, rating)}"
+
+    if (https) {
+      s"https://secure${base_url}"
+    } else {
+      s"http://www${base_url}"
+    }
+  }
 
 }
--- src/test/scala/slavatar/CoreSpec.scala
+++ src/test/scala/slavatar/CoreSpec.scala
@@ -4,9 +4,23 @@ import org.specs2.mutable._
 
 class CoreSpec extends Specification {
 
-  "slavatar" should {
-    "" in {
-      success
+  "basics" should {
+
+    import Core._
+
+    "Clavatar core functionality" in {
+
+      gravatar(email = "i@raynes.me") mustEqual "https://secure.gravatar.com/avatar/54222b6321f0504e0a312c24e97adfc1.jpg?s=50&r=pg&d=retro&"
+
+      gravatar(email = "i@raynes.me", size = 500) mustEqual "https://secure.gravatar.com/avatar/54222b6321f0504e0a312c24e97adfc1.jpg?s=500&r=pg&d=retro&"
+
+      gravatar(email = "i@raynes.me", size = 500, rating = "g") mustEqual "https://secure.gravatar.com/avatar/54222b6321f0504e0a312c24e97adfc1.jpg?s=500&r=g&d=retro&"
+
+      gravatar(email = "i@raynes.me", size = 500, rating = "r") mustEqual "https://secure.gravatar.com/avatar/54222b6321f0504e0a312c24e97adfc1.jpg?s=500&r=r&d=retro&"
+
+      gravatar(email = "i@raynes.me", default = "mm") mustEqual "https://secure.gravatar.com/avatar/54222b6321f0504e0a312c24e97adfc1.jpg?s=50&r=pg&d=mm&"
+
+      gravatar(email = "b.ghose@gmail.com", default = "mm", size = 128, https = false) mustEqual "http://www.gravatar.com/avatar/8aa4490274249db8981283bdadb2ec2b.jpg?s=128&r=pg&d=mm&"
     }
   }
 
